dbf4e9abbe96c2fa0679a88aaecf9db477a0eda4
P3run <P3run@users.noreply.github.com>
2024-03-10 18:20:28 +0100

    Przyspieszono animacje rozwijania/zwijania

--- public/js/arf.js
+++ public/js/arf.js
@@ -2,7 +2,7 @@ var margin = [20, 120, 20, 140],
     width = 1280 - margin[1] - margin[3],
     height = 800 - margin[0] - margin[2],
     i = 0,
-    duration = 1250,
+    duration = 750,
     root;
 
 var tree = d3.layout.tree()
